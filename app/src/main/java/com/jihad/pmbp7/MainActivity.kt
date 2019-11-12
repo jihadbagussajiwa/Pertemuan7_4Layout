3c8ad159afbf41b973cd33b3bbf92f5e2761358a
Saodah1202 <oodah.12@gmail.com>
2019-11-12 23:45:35 +0700

    first commit

---
+++ app/src/main/java/com/jihad/pmbp7/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.jihad.pmbp7
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val THE_MESSAGE = "com.jihad.pmbp7"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun sendMessage(view: View) {
+
+        val x = findViewById<EditText>(R.id.editTxtUsername)
+        val message = x.text.toString()
+
+        val intent = Intent(this, Layout2::class.java).apply {
+            putExtra(THE_MESSAGE,message)
+
+        }
+        startActivity(intent)
+    }
+}
